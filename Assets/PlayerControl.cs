7427ae06db04adbf1d4935c9a64227b944b89e8d
Umut Ucak <umutucak@proton.me>
2023-06-15 22:33:02 +0200

    common input-movement implementation

--- Assets/PlayerControl.cs
+++ Assets/PlayerControl.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-    float velocity = 10;
+    float speed = 10;
     // Update is called once per frame
     void Update()
     {
         if (Input.anyKey)
         {
-            Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
-            Vector3 movement = input.normalized * velocity;
-            transform.position += movement * Time.deltaTime;
+            Vector3 movement = MovementInput();
+            //transform.position += movement * Time.deltaTime;
+            transform.Translate(movement);
         }
+    }
+
+    Vector3 MovementInput()
+    {
+        Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        Vector3 direction = input.normalized;
+        Vector3 velocity = direction * speed;
+        Vector3 movement = velocity * Time.deltaTime;
+
+        return movement;
     }
 }
